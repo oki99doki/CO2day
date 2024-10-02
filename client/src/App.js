dd76c5494e1cc90d606a56048d7ba95b8f4bcb04
Stefan Klinkert <s.klinkert@gmail.com>
2024-10-02 09:37:26 -0400

    update ReactTree.drawio and .png files, clean up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import { Switch, Route } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <Router>
-//       <NavBar user={user} setUser={setUser} />
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/users" element={<UserPage />} />
-//         <Route path="/users/:id" element={<UserPage />} />
-//         <Route path="/new-user-form" element={<NewUserForm />} />
-        
-//         <Route path="/houses" element={<HousesList />} />
-//         <Route path="/cars" element={<CarsList />} />
-//         <Route path="/flights" element={<FlightsList />} />
-//         <Route path="/flights" element={<FlightsList />} />
-        
-        
-        
-        
-//         {/*<Route path="/projects" element={<ProjectPage />} />
-//         <Route path="/projects/:id" element={<ProjectPage/>} />
-//         <Route path="/new-project-form" element={<NewProjectForm />} />
-//         <Route path="/interests" element={<InterestList />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<SignUpForm />} /> */}
-        
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-
 
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
@@ -41,7 +6,7 @@ import Header from "./components/Header";
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(true)
+  const [darkMode, setDarkMode] = useState(false)
 
   const updateDarkMode = () => {
     setDarkMode(prevDarkMode => !prevDarkMode)
